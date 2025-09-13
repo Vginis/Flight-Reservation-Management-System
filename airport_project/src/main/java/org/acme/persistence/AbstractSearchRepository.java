cc138183596417911e6fe58ad849f4957670ee8e
Vaggelis Gkinis <evangelos.gkinis@eurodyn.com>
2025-09-13 16:29:27 +0300

    Implement AbstractSearchRepository for common repository functionalities

---
+++ airport_project/src/main/java/org/acme/persistence/AbstractSearchRepository.java
@@ -0,0 +1,35 @@
+package org.acme.persistence;
+
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import io.quarkus.panache.common.Parameters;
+import io.quarkus.panache.common.Sort;
+import org.acme.search.PageQuery;
+import org.acme.search.PageResult;
+import org.acme.search.SearchQueryCallback;
+
+import java.util.Optional;
+
+public class AbstractSearchRepository<S> implements PanacheRepositoryBase<S, Integer> {
+    public PageResult<S> searchPage(PageQuery query, SearchQueryCallback searchQueryCallback){
+        StringBuilder queryBuilder = new StringBuilder("1 = 1");
+        Parameters parameters = new Parameters();
+        searchQueryCallback.search(query, queryBuilder, parameters);
+
+        Sort sort = Sort.empty();
+        if (query.getSortBy() != null && query.getSortDirection()!=null) {
+            sort = toSort(query.getSortBy().value(),query.getSortDirection().value()).orElse(Sort.empty());
+        }
+
+        searchQueryCallback.defaultSort(sort);
+        PanacheQuery<S> querySearch = this.find(queryBuilder.toString(), sort, parameters).page(query.getIndex(), query.getSize());
+        long total = this.find(queryBuilder.toString(), parameters).count();
+        return new PageResult<>(total, querySearch.list());
+    }
+
+    public Optional<Sort> toSort(String sortBy, String sortDirection) {
+        Sort.Direction direction = (sortDirection.equals("asc")) ? Sort.Direction.Ascending : Sort.Direction.Descending;
+        Sort sort = Sort.by(sortBy, direction);
+        return Optional.of(sort);
+    }
+}
