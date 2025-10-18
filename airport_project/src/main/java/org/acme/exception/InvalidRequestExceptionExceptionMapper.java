b7110b685235ff0205c590f1821fed3f2c124894
vginis <vginis10@gmail.com>
2025-10-18 12:45:45 +0300

    Fix Invalid Request exception response

--- airport_project/src/main/java/org/acme/exception/InvalidRequestExceptionExceptionMapper.java
+++ airport_project/src/main/java/org/acme/exception/InvalidRequestExceptionExceptionMapper.java
@@ -3,13 +3,14 @@ package org.acme.exception;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
+import org.acme.representation.MessageRepresentation;
 
 @Provider
 public class InvalidRequestExceptionExceptionMapper implements ExceptionMapper<InvalidRequestException> {
     @Override
     public Response toResponse(InvalidRequestException exception) {
         return Response.status(400)
-                .entity(exception.getMessage())
+                .entity(new MessageRepresentation(exception.getMessage()))
                 .build();
     }
 }
