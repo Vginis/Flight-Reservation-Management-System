4892d46df0c636f08d3619643f9424e27fc6e896
vginis <vginis10@gmail.com>
2025-10-19 18:37:45 +0300

    Implement administration side bar

--- airport-frontend/src/components/main/administration/administration.component.ts
+++ airport-frontend/src/components/main/administration/administration.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-import { UsersTableComponent } from './users-table/users-table.component';
+import { UsersTableComponent } from './users/users-table/users-table.component';
+import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { AirportsTableComponent } from './airports/airports-table/airports-table.component';
 
 @Component({
   selector: 'app-administration',
   imports: [
-    UsersTableComponent
+    UsersTableComponent,
+    AirportsTableComponent,
+    CommonModule,
+    MatListModule,
+    MatIconModule
   ],
   templateUrl: './administration.component.html',
   styleUrl: './administration.component.css'
 })
 export class AdministrationComponent {
+  selectedSection: string | null = 'users';
 
+  menuOptions = [
+    { key:'users', label:'Users', icon: 'person'},
+    { key: 'airports', label: 'Airports', icon: 'location_on'}
+  ]
+
+  selectSection(option: any): void {
+    this.selectedSection = option.key;
+  }
 }
